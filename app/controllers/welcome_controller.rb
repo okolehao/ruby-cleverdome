afa5de01ede2e0d6c63c51cf8839ce31c2372653
Sanyo Gorbunov <sanyo.gorbunov@gmail.com>
2014-03-28 20:39:52 +0200

    Method to call widgets. Default sessionID. Sandbox.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,21 +1,22 @@
 require "savon"
 
 class WelcomeController < ApplicationController
+  def widgets_call(client, method, locals)
+  	response = client.call(
+		method,
+		:attributes => { 'xmlns' => 'http://tempuri.org/' }, 
+		message: { sessionID: 'EE282ABB-7BC3-42D3-BE98-9F8CE4217A12' } .merge(locals) )
+  end
+
   def index
   	client = Savon.client(
-    	wsdl: 'http://win7dev6.unitedplanners.com/CDWidgets/Services/Widgets.svc?wsdl',
+    	wsdl: 'http://sandbox.cleverdome.com/CDWidgets/Services/Widgets.svc?wsdl',
     	# proxy: 'http://127.0.0.1:8888',
     	element_form_default: :unqualified,
-   		#log_level: :debug
+   		# log_level: :debug
    	)
 
-	response = client.call(
-		:get_document_template,
-		:attributes => { 'xmlns' => 'http://tempuri.org/' }, 
-		message: {
-			sessionID: 'AB1AD8C1-2480-47D7-A45E-489C52146B9E',
-			docGUID: 'AB1AD8C1-2480-47D7-A45E-489C52146B9E'
-		})
+	response = widgets_call(client, :get_document_template, { documentGuid: '1d50dde0-4cd9-11e3-8879-1239b5f79600' })
 	render text: response.hash.inspect
   end
 end
