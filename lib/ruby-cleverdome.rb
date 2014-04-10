e5638acb8bc0af2f445cec08344607f6de6e27d7
EC2 Default User <ec2-user@ip-10-147-243-171.ec2.internal>
2014-04-10 19:41:04 +0000

    we can now call the methods from the gem

--- lib/ruby-cleverdome.rb
+++ lib/ruby-cleverdome.rb
@@ -4,7 +4,10 @@ require 'uuid'
 require 'signed_xml'
 
 class RubyCleverdome
-		def initialize(sso_endpoint, widgets_wsdl)
+		def self.initialize()
+			#hard coded endpoints could be passed
+			sso_endpoint = 'http://sandbox.cleverdome.com/CDSSOService/SSOService.svc/SSO'
+			widgets_wsdl = 'http://sandbox.cleverdome.com/CDWidgets/Services/Widgets.svc?wsdl'
 			@sso_client = Savon.client(
 	  			endpoint: sso_endpoint,
 	  			namespace: 'urn:up-us:sso-service:service:v1',
@@ -19,28 +22,38 @@ class RubyCleverdome
 			)
 		end
 
-	  	def auth(provider, uid, private_key_file, certificate_file)
-	  		req = create_request(provider, uid)
+	  	def self.auth()
+			#hard coded creds could be passed
+			provider = 'http://tempuri.org/'
+			uid = 4898
+			private_key_file = Dir.pwd + '/cert/certificate.pem'
+			certificate_file = Dir.pwd + '/cert/certificate.pem'
 
-			req = sign_request(req, private_key_file, certificate_file)
 
-			resp = saml_call(req)
+	  		req = self.create_request(provider, uid)
+
+			req = self.sign_request(req, private_key_file, certificate_file)
+
+			resp = self.saml_call(req)
 			resp_doc = Nokogiri::XML::Document.parse(resp)
 	  		resp_doc.remove_namespaces!
-			check_resp(resp_doc)
+			self.check_resp(resp_doc)
 
 			session_id = resp_doc.xpath('//Assertion//AttributeStatement//Attribute[@Name="SessionID"]//AttributeValue')[0].content
-			session_id
+
+			response = self.widgets_call(:get_document_template, { documentGuid: '1d50dde0-4cd9-11e3-8879-1239b5f79600', sessionID: session_id })
+
+			response
 	  	end
 
-		def widgets_call(method, locals)
+		def self.widgets_call(method, locals)
 	  		response = @widgets_client.call(
 				method,
 				:attributes => { 'xmlns' => 'http://tempuri.org/' }, 
 				message: { sessionID: 'EE282ABB-7BC3-42D3-BE98-9F8CE4217A12' } .merge(locals) )
 	  	end
 
-		def create_request(provider, uid)
+		def self.create_request(provider, uid)
 			builder = Nokogiri::XML::Builder.new do |xml|
 				xml['s'].Envelope('xmlns:s' => 'http://schemas.xmlsoap.org/soap/envelope/') {
 					xml['s'].Header {
@@ -98,7 +111,7 @@ class RubyCleverdome
 			builder.to_xml
 		end
 
-		def sign_request(xml, private_key_file, certificate_file)
+		def self.sign_request(xml, private_key_file, certificate_file)
 			doc = SignedXml::Document(xml)
 			private_key = OpenSSL::PKey::RSA.new(File.new private_key_file)
 			certificate = OpenSSL::X509::Certificate.new(File.read certificate_file)
@@ -106,7 +119,7 @@ class RubyCleverdome
 			doc.to_xml
 		end
 
-	  	def saml_call(req)
+	  	def self.saml_call(req)
 	  		@sso_client.call( 'GetSSO', soap_action: 'urn:up-us:sso-service:service:v1/ISSOService/GetSSO', xml: req ).to_xml
 	  	end
 
@@ -114,7 +127,7 @@ class RubyCleverdome
 			puts "Hey Justin this worked"
 		end
 
-	  	def check_resp(resp_doc)
+	  	def self.check_resp(resp_doc)
 	  		status = resp_doc.xpath('//Status//StatusCode')[0]['Value']
 
 	  		if status.casecmp('urn:oasis:names:tc:SAML:2.0:status:Success') != 0
@@ -122,5 +135,4 @@ class RubyCleverdome
 	  		end
 	  	end
 
-		private :create_request, :sign_request, :saml_call, :check_resp
 end
